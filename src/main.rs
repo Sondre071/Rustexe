b0e875f20407194c70642674ce752fa70eedbbc4
Sondre071 <sirciller@gmail.com>
2025-11-26 23:36:56 +0100

    Step 1.2: Add parameter parsing with Clap

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
+use clap::Parser;
+
+// Search for a pattern in a file and displa ythe lines that contain it
+#[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let args = Cli::parse();
 
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
-
-    println!(
-        "Hello, world!, pattern: {:?}, path: {:?}",
-        args.pattern, args.path
-    );
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
